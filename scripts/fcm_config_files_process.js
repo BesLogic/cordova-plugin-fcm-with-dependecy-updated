bd749278fd87ad2bc32991aee3d57ecd83f5c623
Carl Montplaisir <carl.montplaisir@Beslogic.com>
2020-04-30 09:35:08 -0400

    Removed ios

--- scripts/fcm_config_files_process.js
+++ scripts/fcm_config_files_process.js
@@ -19,22 +19,10 @@ fs.ensureDirSync = function (dir) {
 var config = fs.readFileSync('config.xml').toString();
 var name = getValue(config, 'name');
 
-var IOS_DIR = 'platforms/ios';
 var ANDROID_DIR = 'platforms/android';
 var ANDROID_DIR_RES_VALUES_DIR = ANDROID_DIR + '/res/values';
 
 var PLATFORM = {
-    IOS: {
-        dest: [
-            IOS_DIR + '/' + name + '/Resources/GoogleService-Info.plist',
-            IOS_DIR + '/' + name + '/Resources/Resources/GoogleService-Info.plist'
-        ],
-        src: [
-            'GoogleService-Info.plist',
-            IOS_DIR + '/www/GoogleService-Info.plist',
-            'www/GoogleService-Info.plist'
-        ]
-    },
     ANDROID: {
         dest: [
             ANDROID_DIR + '/google-services.json',
@@ -51,9 +39,6 @@ var PLATFORM = {
 };
 
 // Copy key files to their platform specific folders
-if (directoryExists(IOS_DIR)) {
-    copyKey(PLATFORM.IOS);
-}
 if (directoryExists(ANDROID_DIR)) {
     copyKey(PLATFORM.ANDROID, updateStringsXml)
 }
